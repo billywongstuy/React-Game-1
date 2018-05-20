36b90d7f1b81a000f63643f515cfed45403af626
Billy Wong <bwong5@stuy.edu>
2018-05-20 00:15:22 -0400

    Creation of table of x size is done

---
+++ README.md
@@ -0,0 +1,8 @@
+# Escape Game
+
+-Start at a tile
+=Have to get to the end tile
+-There are walls
+-There are some amount of patrollers
+-If patroller sees you, sent back to beginning
+-Limited number of moves
---
+++ src/index.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//each square be can clicked on for more info
+
+class Square extends React.Component {
+    constructor(props) {
+	super(props);
+    }
+
+    render() {
+	return (
+		<button className="square">
+		{this.props.value}
+	    </button>
+	)
+    }
+}
+
+class Board extends React.Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    squares: Array(this.props.size).fill(Array(this.props.size).fill(null)),
+	}
+	this.renderSquare = this.renderSquare.bind(this);
+    }
+    
+    renderSquare(i) {
+	return <Square value={i} />;
+    }
+
+    renderRow(row,rowNum) {
+	return row.map((square,squareNum) => {
+	    return (
+		    <td key={squareNum}>
+		    {this.renderSquare(square + " " + rowNum + "," + squareNum)}
+		</td>
+	    );
+	});
+    }
+    
+    render() {
+	const squares = this.state.squares;
+	console.log(squares);
+
+	const items = squares.map((row,rowNum) => {
+	    return (
+		    <tr key={rowNum}>
+		    {this.renderRow(row,rowNum)}
+		</tr>
+	    )
+	});
+	
+	return (
+		<table>
+		<tbody>
+		{items}
+	    </tbody>
+	    </table>
+	)
+    }
+}
+
+class Game extends React.Component {
+    constructor(props) {
+	super(props);
+    }
+    
+    render() {
+	return (
+		<Board size={this.props.size}/>
+	)
+    }
+}
+
+ReactDOM.render(
+	<Game size={4}/>, document.getElementById('root')
+);
+
+//npm start
